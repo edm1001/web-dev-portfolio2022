08b3bd90270f8247b1295726f8c814fd0a588ba9
Edmer Valencia <edmer_franciz@yahoo.com>
2022-08-18 11:29:33 -0500

    index files for components

---
+++ src/Pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import About from '../Components/About';
+import Projects from '../Components/Projects';
+import Contact from '../Components/Contact';
+
+
+export const Home = () => {
+  return (
+    <main>
+    <About/>
+    <Contact/>
+    <Projects/>
+    </main>
+  )
+}
+
+
+export default Home;
