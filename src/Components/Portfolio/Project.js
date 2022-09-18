15da6fae3d1cc1875f2ed336aa2b4ea0cc3f7618
Edmer Valencia <edmer_franciz@yahoo.com>
2022-09-18 12:33:11 -0500

    add logo for projects

--- src/Components/Portfolio/Project.js
+++ src/Components/Portfolio/Project.js
@@ -2,7 +2,11 @@ import React from 'react'
 import {BiLinkExternal} from 'react-icons/bi';
 import travelDiary from '../../Assets/gifs/travel-blog.gif';
 import Chatterize from '../../Assets/gifs/chatterize.gif';
-
+import {IoLogoJavascript} from 'react-icons/io';
+import {SiExpress, SiMongodb, SiInsomnia, SiSequelize, } from 'react-icons/si';
+import {FaReact} from 'react-icons/fa';
+import {DiNodejs} from 'react-icons/di';
+import {GrHeroku , GrMysql} from 'react-icons/gr';
 // import {BsGlobe} from 'react-icons/bs';
 
 function Projects() {
@@ -14,14 +18,29 @@ function Projects() {
     <div className='projects' >
     <div id='project1'>
     <img src={travelDiary} alt ='project01' href='https://afternoon-harbor-62644.herokuapp.com/' id='travel-gif' />
-    {/* add logo instead of name */}
-    <p> JavaScript, HTML, CSS, HandleBars, Multer</p>
+    <h3> MVS Framework</h3>
+    <p> 
+    <SiInsomnia size={20}/>
+    <SiSequelize size={20}/>
+    <GrMysql size={20}/>
+    <GrHeroku size={20}/>
+    <DiNodejs size={20}/>
+    <IoLogoJavascript size={25}/>
+    </p>
     <a href='https://afternoon-harbor-62644.herokuapp.com/' ><BiLinkExternal size={25}/></a>
     </div>
     <div id='project2'> 
     <img src={Chatterize} alt ='project02' id='chatterize'/>
-    <h4>Chatterize!</h4>
-    <p> React, HTML, CSS, JavaScript</p>
+    <h3>MERN Stack</h3>
+    <p> 
+    <SiMongodb size={20}/>
+    <SiExpress size={20}/>
+    <FaReact size={20}/>
+    <DiNodejs size={20}/>
+    <IoLogoJavascript size={25}/>
+    <GrHeroku size={20}/>
+    
+    </p>
     <a href='https://chatterize123.herokuapp.com/'><BiLinkExternal size={20}/></a>
     </div>
     
